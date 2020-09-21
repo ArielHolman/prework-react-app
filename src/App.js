379a8f577f5bf9742d1544081dab69149d0cc50b
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-09-21 10:20:44 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './firstComponent'
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: '',
+      cars: [],
+      houses: []
+    };
+  }
+
+  // toggle = () => {
+  //   this.setState({isOn: !this.state.isOn})
+  // }
+
+  inputUpdate = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  formSubmit = (event) => {
+    event.preventDefault()
+    // console.log("**THIS IS STATE", this.state.items)
+    this.setState({
+      items : [...this.state.cars, this.state.houses, this.state.input],
+      input: ''
+    })
+  }
+
+  render() {
+    // console.log("**THIS STUPID STATE***", this.state.isOn)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World</h1>
+        <form onSubmit={this.formSubmit}>
+          <input value={this.state.items} onChange={this.inputUpdate}/>
+          <button>List Houses</button>
+        </form>
+        <form onSubmit={this.formSubmit}>
+          <input value={this.state.items} onChange={this.inputUpdate}/>
+          <button>List Cars</button>
+        </form>
+        <FirstComponent items={this.state.cars} />
+        <FirstComponent items={this.state.houses} />
       </header>
     </div>
   );
+ }
 }
 
 export default App;
+
+//  <FirstComponent/>
